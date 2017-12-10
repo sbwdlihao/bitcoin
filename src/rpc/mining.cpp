42109310696d086f256ae6658a2a96a540280bd7
sbwdlihao <sbwdlihao@gmail.com>
2017-12-10 14:41:14 +0800

    update rpc miner

--- src/rpc/mining.cpp
+++ src/rpc/mining.cpp
@@ -30,6 +30,7 @@
 #include <stdint.h>
 
 #include <univalue.h>
+#include "crypto/equihash.h"
 
 unsigned int ParseConfirmTarget(const UniValue& value)
 {
@@ -118,6 +119,8 @@ UniValue generateBlocks(std::shared_ptr<CReserveScript> coinbaseScript, int nGen
     }
     unsigned int nExtraNonce = 0;
     UniValue blockHashes(UniValue::VARR);
+    unsigned int n = Params().EquihashN();
+    unsigned int k = Params().EquihashK();
     while (nHeight < nHeightEnd)
     {
         std::unique_ptr<CBlockTemplate> pblocktemplate(BlockAssembler(Params()).CreateNewBlock(coinbaseScript->reserveScript));
@@ -128,8 +131,38 @@ UniValue generateBlocks(std::shared_ptr<CReserveScript> coinbaseScript, int nGen
             LOCK(cs_main);
             IncrementExtraNonce(pblock, chainActive.Tip(), nExtraNonce);
         }
-        while (nMaxTries > 0 && pblock->nNonce < nInnerLoopCount && !CheckProofOfWork(pblock->GetHash(), pblock->nBits, Params().GetConsensus())) {
+
+        // Hash state
+        crypto_generichash_blake2b_state eh_state;
+        EhInitialiseState(n, k, eh_state);
+        // I = the block header minus nonce and solution.
+        CEquihashInput I{*pblock};
+        CDataStream ss(SER_NETWORK, PROTOCOL_VERSION);
+        ss << I;
+
+        // H(I||...
+        crypto_generichash_blake2b_update(&eh_state, (unsigned char*)&ss[0], ss.size());
+
+        while (nMaxTries > 0 && pblock->nNonce < nInnerLoopCount) {
             ++pblock->nNonce;
+
+            // H(I||V||...
+            crypto_generichash_blake2b_state curr_state;
+            curr_state = eh_state;
+            crypto_generichash_blake2b_update(&curr_state,
+                                              (unsigned const char*)&pblock->nNonce,
+                                              sizeof(uint32_t));
+
+            // (x_1, x_2, ...) = A(I, V, n, k)
+            std::function<bool(std::vector<unsigned char>)> validBlock =
+                [&pblock](std::vector<unsigned char> soln) {
+                  pblock->nSolution = soln;
+                  return CheckProofOfWork(pblock->GetHash(), pblock->nBits, Params().GetConsensus());
+                };
+            bool found = EhBasicSolveUncancellable(n, k, curr_state, validBlock);
+            if (found) {
+                goto endloop;
+            }
             --nMaxTries;
         }
         if (nMaxTries == 0) {
@@ -138,6 +171,7 @@ UniValue generateBlocks(std::shared_ptr<CReserveScript> coinbaseScript, int nGen
         if (pblock->nNonce == nInnerLoopCount) {
             continue;
         }
+endloop:
         std::shared_ptr<const CBlock> shared_pblock = std::make_shared<const CBlock>(*pblock);
         if (!ProcessNewBlock(Params(), shared_pblock, true, nullptr))
             throw JSONRPCError(RPC_INTERNAL_ERROR, "ProcessNewBlock, block not accepted");
